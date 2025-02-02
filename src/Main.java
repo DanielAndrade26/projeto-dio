353f0fd229039b3b807ca79b8b9fcd4f1c9f17f2
daniel andrade <deivison2000d@gmail.com>
2025-02-02 13:05:35 -0300

    mudança de bem vindo para ola mundo

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("bem vindo");
+        System.out.println("ola mundo");
+
     }
 }
 
